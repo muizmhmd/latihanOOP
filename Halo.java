faa7b2f6f47f8fe04046bf8a0d016b3776216911
muizmhmd <muiz9muhamad@gmail.com>
2019-03-07 17:35:28 +0700

    pertama

---
+++ Halo.java
@@ -0,0 +1,7 @@
+public class Halo{
+	public static void main(String[] args){
+		System.out.println("Nama  : Muhamad A. Muiz");
+		System.out.println("NIM   : 17090150");
+		System.out.println("Kelas : 4A");
+	}
+}
